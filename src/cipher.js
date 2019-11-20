e9ad908808e0723377f294137abf46c8b0ea0c3e
“ievastumpe” <ieva.stumpe@gmail.com>
2019-11-20 12:56:38 -0300

    cambios

--- src/cipher.js
+++ src/cipher.js
@@ -1,9 +1,41 @@
 window.cipher = {
-  encode: () => {
-    /* Acá va tu código */
+  encode: (infonumero, infotext) => { 
+    let palabra;
+    let resultadoLetras= "";
+
+  for(let i=0; i < infotext.length; i++){ //recorre el texto y vea los posiciones
+    let resultadoNumero;
+    palabra = infotext.charCodeAt(i);
+
+    if(palabra >=65 && palabra <= 90 ){ //solo mayusculas
+      resultadoNumero = (palabra - 65 + parseInt(infonumero))% 26 + 65; //transformacion a Ascii en numero
+      resultadoLetras = resultadoLetras + String.fromCharCode(resultadoNumero); //transformacion a letras
+    }
+
+    if (palabra ===32){
+      resultadoLetras += " "
+    }
+    }
+    return resultadoLetras;
+  },    
+
+
+  decode: (infonumero, infotext) => {
+    let palabra;
+    let resultadoLetras= "";
     
-  },
-  decode: () => {
-    /* Acá va tu código */
-  }
-};
+    for(let i=0; i < infotext.length; i++){ //recorre el texto y vea los posiciones
+      let resultadoNumeroDecifrado;
+      palabra = infotext.charCodeAt(i);
+    
+      if(palabra >=65 && palabra <= 90 ){ //solo mayusculas
+        resultadoNumeroDecifrado = (palabra - 90 - parseInt(infonumero))% 26 + 90; //transformacion a Ascii en numero
+        resultadoLetras = resultadoLetras + String.fromCharCode(resultadoNumeroDecifrado); //transformacion a letras
+    }
+      
+    }
+  
+  return resultadoLetras;
+}
+
+}
--- src/index.js
+++ src/index.js
@@ -1,1 +1,49 @@
-/*Acá va tu código*/
+
+//CIFRAR
+let infotextdeci = document.getElementById("output");//resultado
+let bottonCifrar = document.getElementById("myBtnCifrar"); //guarde botton para Cifrar
+bottonCifrar.addEventListener("click", enviartexto); //dar evento a botton Cifrar
+
+function enviartexto(){
+  let infotext=document.getElementById("textInput").value; //guardar los datos del texto ingresado
+  let infonumero=document.getElementById("shift").value; //guardar el numero de desplazamiento del texto cifrado o decifrado
+  let infooutput = cipher.encode(infonumero, infotext);//resultado en cipher.js
+
+  infotextdeci.innerHTML = infooutput; 
+  }
+
+
+//DECIFRAR
+let bottonDecifrar = document.getElementById("myBtnDecifrar"); //guarde botton Decifrar
+bottonDecifrar.addEventListener("click", decifrartexto); //dar evento a botton Decifrar
+
+function decifrartexto(){
+  let infotext=document.getElementById("textInput").value; //guardar los datos del texto ingresado
+  let infonumero = document.getElementById("shift").value; //guardar el numero de desplazamiento del texto cifrado o decifrado
+  let infooutput=window.cipher.decode(infonumero, infotext); //mostrar el resultado del texto cifrado o decifrado
+
+  infotextdeci.innerHTML = infooutput; 
+  }
+
+
+//BOTON para COPPYING 
+let bottonCopiar = document.getElementById("myBtnCopiar"); //guarde botton Copiar
+bottonCopiar.addEventListener("click", copyText); //dar evento a botton Copiar
+
+function copyText() {
+    var texttocoppy=document.getElementById("output");
+    texttocoppy.value; 
+    texttocoppy.select();
+    document.execCommand("copy");
+    alert("Copied the text: " + texttocoppy.value);
+    }
+  
+//BOTON para ERASING
+let bottonLimpiar = document.getElementById("myBtnLimpiar"); //guarde botton Copiar
+bottonLimpiar.addEventListener("click", eraseText); //dar evento a botton Copiar
+
+  function eraseText() {
+    document.getElementById("output").value = "";
+    document.getElementById("textInput").value="";
+    location.reload();
+  }
